2a855191a5f9b9718700ebd9a99625f93b810a52
Bruno Hubner <brunohubnerdev@gmail.com>
2021-12-07 15:23:17 -0300

    Fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
     return (
-        <div className="wrapper">
+        <div className="wrapper app">
             <AuthInputProvider>
                 <AuthProvider>
                     <AuthOrApp>
